23062d6e80eb8cc7468f0c75ba46e20f8cc846f2
saravanan <sarwana007@gmail.com>
2024-04-28 21:45:16 +0530

    logic updated

--- src/FlippingAnImage.java
+++ src/FlippingAnImage.java
@@ -1,16 +1,11 @@
 public class FlippingAnImage {
-    static List<List<Integer>> solve(List<List<Integer>>  input){
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        for(List<Integer> list1 : input) {
-            List<Integer> inner = new ArrayList<Integer>();
-            for(Integer list2 : list1) {
-                inner.add(list2 == 1 ? 0 : 1);
+    public int[][] flipAndInvertImage(int[][] image) {
+        int len = image.length;
+        int[][] res = new int[len][len];
+        for(int i = 0; i < image.length; i++) {
+            for(int j = 0; j < image[i].length; j++) {
+                res[i][j] = 1 - image[i][len - j - 1];
             }
-            List<Integer> inner1 = new ArrayList<Integer>();
-            for(int i = inner.size()-1; i >= 0; i--) {
-                inner1.add(inner.get(i));
-            }
-            res.add(inner1);
         }
         return res;
     }
