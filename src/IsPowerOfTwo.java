f4eff0a09157a5c88e15427a7b6a0dc0709d841f
saravanan <sarwana007@gmail.com>
2024-05-05 19:15:33 +0530

    PowerOfTwo

---
+++ src/IsPowerOfTwo.java
@@ -0,0 +1,8 @@
+class IsPowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0 || n < 0) {
+            return false;
+        }
+        return n != 0 && ((n & (n-1)) == 0);
+    }
+}
