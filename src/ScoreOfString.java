5ee7f1a243ba3c2b270316a9724e62bf99ef0092
saravanan <sarwana007@gmail.com>
2024-05-12 17:17:48 +0530

    Score of a String

---
+++ src/ScoreOfString.java
@@ -0,0 +1,11 @@
+class ScoreOfString {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for(int i = 0; i < s.length() - 1; i++) {
+            char a = s.charAt(i);
+            char b = s.charAt(i+1);
+            sum = sum + Math.abs(a - b);
+        }
+        return sum;
+    }
+}
