f1669f24cc4020688a8a8affa1f3c7d02731e8ce
saravanan <sarwana007@gmail.com>
2024-05-12 17:49:26 +0530

    Reverse Prefix of Word

---
+++ src/ReversePrefix.java
@@ -0,0 +1,9 @@
+class ReversePrefix {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if(index != -1) {
+            return new StringBuilder(word.substring(0, index + 1)).reverse().toString() + word.substring(index + 1);
+        }
+        return word;
+    }
+}
