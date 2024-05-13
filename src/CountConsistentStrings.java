20cbbf03f11224ce82097b8274d49f5373897593
saravanan <sarwana007@gmail.com>
2024-05-14 00:32:57 +0530

    1684. Count the Number of Consistent Strings

---
+++ src/CountConsistentStrings.java
@@ -0,0 +1,18 @@
+class CountConsistentStrings {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = words.length;
+        int[] store = new int[26];
+        for(char c : allowed.toCharArray()) {
+            store[c - 'a']++;
+        }
+        for(String word : words) {
+            for(char c : word.toCharArray()) {
+                if(store[c - 'a'] <= 0) {
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
