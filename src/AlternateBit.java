9f873850e2ef3a81ad097d6bf6ad7f0ba867ae51
saravanan <sarwana007@gmail.com>
2024-05-21 00:08:44 +0530

    Check if a number has bits in an alternate pattern.

---
+++ src/AlternateBit.java
@@ -0,0 +1,15 @@
+public class AlternateBit{
+    static boolean check(int n){
+		int prev = n % 2;
+        n = n / 2;
+        while(n > 0) {
+            int curr = n % 2;
+            if(curr == prev) {
+                return false;
+            }
+            prev = curr;
+            n = n / 2;
+        }
+        return true;
+    }
+}
