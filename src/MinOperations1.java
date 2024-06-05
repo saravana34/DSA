3f271db63b8faeef84266bf886b91ead209d6af1
saravanan <sarwana007@gmail.com>
2024-06-05 22:56:06 +0530

    1598. Crawler Log Folder

---
+++ src/MinOperations1.java
@@ -0,0 +1,17 @@
+class MinOperations {
+    public int minOperations(String[] logs) {
+        Stack st1=new Stack();
+        for(int i=0;i<logs.length;i++){
+            if(logs[i].equals("../")){
+                if(!st1.isEmpty()){
+                    st1.pop();
+                }
+            } else if(logs[i].equals("./")){
+                //Eat Five Star Do Nothing
+            } else {
+                st1.push(logs[i]);
+            }
+        }
+        return st1.size();
+    }
+}
