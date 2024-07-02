9f53bd629092e355096110744e47e3f109891999
saravanan <sarwana007@gmail.com>
2024-07-03 01:44:13 +0530

    168. Excel Sheet Column Title

---
+++ src/ConvertToTitle.java
@@ -0,0 +1,12 @@
+class ConvertToTitle {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while(columnNumber > 0) {
+            columnNumber--;
+            int curr = columnNumber % 26;
+            columnNumber /= 26;
+            sb.append((char) (curr + 'A'));
+        }
+        return sb.reverse().toString();
+    }
+}
