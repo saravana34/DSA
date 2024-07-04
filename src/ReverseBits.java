993649ff69d9056b02ebc16c8993f9e23febf562
saravanan <sarwana007@gmail.com>
2024-07-04 23:40:25 +0530

    ReverseBits - approach 2

--- src/ReverseBits.java
+++ src/ReverseBits.java
@@ -1,10 +1,19 @@
 public class ReverseBits{
     static long solve(String s){
-		long res = 0;
+	long res = 0;
         for(int i = 31; i >= 0; i--) {
             res <<= 1;
             res += (s.charAt(i) - '0') % 2;
         }
         return res;
+	/*
+ 	int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+ 	*/
     }
 }
