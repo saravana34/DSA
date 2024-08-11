04a20b9932bbf61f895824ff760b0df2b110ce48
saravanan <sarwana007@gmail.com>
2024-08-11 19:18:12 +0530

    Update FindMin.java

--- src/search/FindMin.java
+++ src/search/FindMin.java
@@ -16,5 +16,19 @@ class FindMin {
             }
         }
         return -1;
+        
+        /*Approch 2
+        if(nums[low] < nums[high]) {
+            return nums[low];
+        }
+        while(low < high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];*/
     }
 }
