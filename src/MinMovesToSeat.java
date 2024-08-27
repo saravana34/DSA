6669c4849ea070b16e8505dbca55f9e6638e27c0
saravanan <sarwana007@gmail.com>
2024-08-27 23:16:58 +0530

    2037. Minimum Number of Moves to Seat Everyone

---
+++ src/MinMovesToSeat.java
@@ -0,0 +1,11 @@
+class MinMovesToSeat {
+    public int minMovesToSeat(int[] seats, int[] students) {
+         Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans=0;
+        for(int i=0;i<seats.length;i++){
+            ans+=Math.abs(students[i]-seats[i]);
+        }
+        return ans;
+    }
+}
