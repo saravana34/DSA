044355dd619adbc6a3966d5cf8722db32c582594
saravanan <sarwana007@gmail.com>
2024-09-10 22:38:01 +0530

    392. Is Subsequence

---
+++ src/IsSubsequence.java
@@ -0,0 +1,13 @@
+class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int sp = 0;
+        int tp = 0;
+        while (sp < s.length() && tp < t.length()) {
+            if (s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+            tp++;
+        }
+        return sp == s.length();  
+    }
+}
