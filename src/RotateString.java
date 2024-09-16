b9afc28cb8144e4a70221a1ec6155f587b7c5b66
saravanan <sarwana007@gmail.com>
2024-09-16 22:25:40 +0530

    796. Rotate String

---
+++ src/RotateString.java
@@ -0,0 +1,7 @@
+class RotateString {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        s += s;
+        return s.contains(goal);
+    }
+}
