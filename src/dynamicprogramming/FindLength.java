5890ee3de6ee692a77f8c454ced8003340523b09
saravanan <sarwana007@gmail.com>
2024-09-21 22:31:31 +0530

    718. Maximum Length of Repeated Subarray

---
+++ src/dynamicprogramming/FindLength.java
@@ -0,0 +1,19 @@
+class FindLength {
+    public int findLength(int[] nums1, int[] nums2) {
+        int n1= nums1.length;
+        int n2 = nums2.length;
+        int[][] dp = new int[n1+1][n2+1];
+        int ans = 0;
+        for(int i = 1; i <= n1; i++) {
+            for(int j = 1; j <= n2; j++) {
+                if(nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = Math.max(ans, dp[i][j]);
+                } else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
