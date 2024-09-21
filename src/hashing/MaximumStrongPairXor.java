005f874aef60e1103fe486c8f28c7086294d7196
saravanan <sarwana007@gmail.com>
2024-09-21 21:40:46 +0530

    2932. Maximum Strong Pair XOR I

---
+++ src/hashing/MaximumStrongPairXor.java
@@ -0,0 +1,13 @@
+class MaximumStrongPairXor {
+    public int maximumStrongPairXor(int[] nums) {
+        int res = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i; j<nums.length; j++){
+                if(Math.abs(nums[i] - nums[j]) <= Math.min(nums[i], nums[j])){
+                    res = Math.max(res, nums[i] ^ nums[j]);
+                }
+            }
+        }
+        return res;
+    }
+}
